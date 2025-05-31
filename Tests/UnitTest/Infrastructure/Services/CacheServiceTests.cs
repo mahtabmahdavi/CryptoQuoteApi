401feae922e3a3c9ee805993c24f3dab1dd5dc77
mahtabmahdavi <mahtaabmahdavi@gmail.com>
2025-05-31 03:31:59 +0330

    Add unit test for CacheService and add a method when cache key exists

---
+++ Tests/UnitTest/Infrastructure/Services/CacheServiceTests.cs
@@ -0,0 +1,50 @@
+﻿using CryptoQuoteApi.Application.Settings;
+using CryptoQuoteApi.Infrastructure.Services;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tests.UnitTest.Infrastructure.TestModels;
+
+namespace Tests.UnitTest.Infrastructure.Services;
+
+[TestClass]
+public class CacheServiceTests
+{
+    private IMemoryCache _memoryCache;
+    private CacheSettings _cacheSettings;
+    private CacheService _cacheService;
+
+    [TestInitialize]
+    public void Setup()
+    {
+        _memoryCache = new MemoryCache(new MemoryCacheOptions());
+        _cacheSettings = new CacheSettings
+        {
+            DefaultExpirationMinutes = 5,
+            SlidingExpirationMinutes = 2,
+            AbsoluteExpirationMinutes = 30
+        };
+
+        var options = Options.Create(_cacheSettings);
+        _cacheService = new CacheService(_memoryCache, options);
+    }
+
+    [TestMethod]
+    public void Get_WhenKeyExists_ReturnsValue()
+    {
+        // Arrange
+        var key = "test_key";
+        var expectedValue = "test_value";
+        _cacheService.Set(key, expectedValue);
+
+        // Act
+        var result = _cacheService.Get<string>(key);
+
+        // Assert
+        Assert.AreEqual(expectedValue, result);
+    }
+}
